2f5bb99b172e40a66934b06122423a656f4fae19
Antonio <antonio.garciamn@gmail.com>
2023-09-12 17:07:58 -0300

    feat: todo entity

---
+++ src/main/java/com/example/todo/entity/Todo.java
@@ -0,0 +1,56 @@
+package com.example.todo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "todos")
+public class Todo {
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String name;
+  private String description;
+  private boolean finish;
+  private int priority;
+  
+  public Long getId() {
+    return id;
+  }
+  public String getName() {
+    return name;
+  }
+  public String getDescription() {
+    return description;
+  }
+  public boolean isFinish() {
+    return finish;
+  }
+  public int getPriority() {
+    return priority;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  public void setDescription(String description) {
+    this.description = description;
+  }
+  public void setFinish(boolean finish) {
+    this.finish = finish;
+  }
+  public void setPriority(int priority) {
+    this.priority = priority;
+  }
+
+  
+
+
+}
